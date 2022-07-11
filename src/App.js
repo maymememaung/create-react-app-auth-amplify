41cc180c544698aee0a5f371d403bcd35d76d8ec
maymememaung <maymememaung27@gmail.com>
2022-07-10 21:51:19 -0400

    test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AmplifySignOut />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
